66e24b80072d20d9ccd5fec8f8e0a1e39c5d150e
rodrigoDesktop <rodrigo.abasto21@gmail.com>
2018-05-29 11:32:13 -0400

    DateTimePicker Parte2

--- app/src/main/java/com/uagrm/rodrigoab/agendakotlin/formulario.kt
+++ app/src/main/java/com/uagrm/rodrigoab/agendakotlin/formulario.kt
@@ -7,20 +7,24 @@ import android.os.Bundle
 import android.support.v7.app.AlertDialog
 import android.view.Menu
 import android.view.MenuItem
-import android.widget.ArrayAdapter
-import android.widget.Toast
+import android.view.View
+import android.widget.*
 import com.uagrm.rodrigoab.agendakotlin.DBHelper.DBHelper
 import kotlinx.android.synthetic.main.activity_formulario.*
+import java.text.SimpleDateFormat
+import java.util.*
 
 class formulario : AppCompatActivity() {
 
     var bundle : Bundle ?= null
+    var formato : SimpleDateFormat ?= null
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_formulario)
         var selectorAdapter = ArrayAdapter.createFromResource(this, R.array.items_colores, android.R.layout.simple_spinner_dropdown_item)
         selector_color.adapter = selectorAdapter
+        formato = SimpleDateFormat("dd-MM-yyyy HH:mm:ss", Locale.getDefault())
 
         bundle = intent.extras
         if ( bundle!!.getInt("id") > 0){
@@ -36,7 +40,44 @@ class formulario : AppCompatActivity() {
             edt_fin.setText(bundle!!.getString("inicio"))
         }
 
+        edt_inicio.setOnClickListener {
+            selectorFecha(edt_inicio)
+        }
+
+        edt_fin.setOnClickListener {
+            selectorFecha(edt_fin)
+        }
+
+
     }
+
+
+    private fun selectorFecha(textView: TextView){
+
+            var alertView = AlertDialog.Builder(this)
+            var vistaAlertDialog = View.inflate(this, R.layout.date_time_picker, null)
+
+            alertView.setView(vistaAlertDialog)
+            val dialogo = alertView.show()
+
+
+            val datePicker2 = vistaAlertDialog.findViewById<DatePicker>(R.id.datepicker)
+            val timePicker2 = vistaAlertDialog.findViewById<TimePicker>(R.id.timepicker)
+
+            timePicker2.setIs24HourView(true)
+
+
+            vistaAlertDialog.findViewById<Button>(R.id.btn_dateTime).setOnClickListener {
+                var fecha = GregorianCalendar(datePicker2.year, datePicker2.month, datePicker2.dayOfMonth, timePicker2.hour, timePicker2.minute, 0)
+                var cadena = formato!!.format(fecha.time).toString()
+
+                textView.text = cadena
+                dialogo.dismiss()
+
+            }
+
+    }
+
 
     private fun getPosicion(color : String): Int{
         when(color) {
